7c8683666dfd46d4e2489efb1ae334076874f743
IgorNSMD <insanmartind@gmail.com>
2023-01-23 15:22:28 -0300

    Capitulo 83

--- model/Category.js
+++ model/Category.js
@@ -2,7 +2,7 @@ import {DataTypes} from 'sequelize'
 
 import db from '../config/db.js'
 
-const Category = db.define('pategories',{
+const Category = db.define('categories',{
     name: {
         type: DataTypes.STRING(50),
         allowNull:false
---
+++ seed/seeder.js
@@ -0,0 +1,66 @@
+import { exit } from 'node:process'
+
+import categories from './categories.js';
+import prices from './prices.js';
+
+import Category from '../model/Category.js';
+import Price from '../model/Price.js';
+
+import db from '../config/db.js'
+
+const importData = async() => {
+    try {
+        // Actualizar BD
+        await db.authenticate()
+        
+        // Generar las columnas
+        await db.sync()
+
+        // Insertar los datos
+        // await Category.bulkCreate(categories)
+        // await Price.bulkCreate(prices)
+        
+        // Ejecutan la inserción al mismo tiempo
+        await Promise.all([
+            Category.bulkCreate(categories),
+            Price.bulkCreate(prices)
+        ])
+
+        console.log('Datos importados correctamente..')
+        exit(0)
+
+    } catch (error) {
+        console.log(error)
+
+        exit(1);
+
+        //process.exit(1)        
+    }
+}
+
+const deleteData = async() => {
+    try {
+
+        await Promise.all([
+            Category.destroy({where:{}, truncate:true}),
+            Price.destroy({where:{}, truncate:true})
+        ])
+
+        //await db.sync({force:true}) - Elimina las tablas y datos
+        
+        console.log('Datos eliminados correctamente..')
+        exit(0);
+        
+    } catch (error) {
+        console.log(error)        
+        exit(1);        
+    }
+}
+
+if(process.argv[2] === "-i"){
+    importData();
+}
+
+if(process.argv[2] === "-d"){
+    deleteData();
+}
