3b873039b848e922545f8471b26991fa30fb3730
IgorNSMD <insanmartind@gmail.com>
2023-01-31 22:37:30 -0300

    Capitulo 156

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,16 +2,44 @@ import { Price, Category, Property } from '../model/index.js'
 
 const start = async (req,res) => {
     
-    const [ categories, prices ] = await Promise.all([
+    const [ categories, prices, houses, departments ] = await Promise.all([
         Category.findAll({ raw: true }),
-        Price.findAll({ raw: true })
+        Price.findAll({ raw: true }),
+        Property.findAll({ 
+            limit: 3,
+            where: {
+                categoryid: 1
+            },
+            include: [
+                {
+                    model: Price, as:'price'
+                }
+            ],
+            order: [
+                ['createdAt','DESC']
+            ]
+         }),
+         Property.findAll({ 
+            limit: 3,
+            where: {
+                categoryid: 2
+            },
+            include: [
+                {
+                    model: Price, as:'price'
+                }
+            ],
+            order: [
+                ['createdAt','DESC']
+            ]
+         })
     ])
 
    // console.log( categories )
 
     res.render('start',{
         pageLabel:'Inicio',
-        categories, prices
+        categories, prices, houses, departments
     })
 }
 
